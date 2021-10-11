25b1c5db336b49767ae60be2b85eaa0bd5b12ab7
CodingBee77 <00martyna.znajdek00@gmail.com>
2021-10-11 12:12:02 +0200

    Create moch chart component in a detailed view

---
+++ frontend/src/components/chart-container.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <line-chart
+      v-if="loaded"
+      :chartdata="chartdata"
+      :options="options"/>
+  </div>
+</template>
+
+<script>
+import LineChart from './chart.vue'
+
+export default {
+  name: 'LineChartContainer',
+  components: { LineChart },
+  data: () => ({
+    loaded: false,
+    chartdata: null
+  }),
+
+  async mounted () {
+    this.loaded = false
+    try {
+      // const { historic_measurements } = await fetch('https://airapi.airly.eu/v2/measurements/nearest?lat=50.062006&lng=19.940984&maxDistanceKM=2&maxResults=1')
+      // historic_measurements_PM1 = historic_measurements["history"][1]["values"][1]["value"]
+      // this.chartdata = historic_measurements
+      this.chartdata = {
+        labels: ['January', 'February'],
+          datasets: [
+            {
+              label: 'Data One',
+              backgroundColor: '#f87979',
+              data: [40, 20]
+            }
+          ]
+      };
+      this.options = {
+        responsive: true,
+        maintainAspectRatio: false
+      };
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
+</script>
--- frontend/src/views/Detail.vue
+++ frontend/src/views/Detail.vue
@@ -1,33 +1,31 @@
 <template>
   <h1>
     Detail view {{ city }}
-              <router-link
-        to="/"
-        tag="v-btn"
-      >
-    <v-btn
-      class="mx-2"
-      fab
-      dark
-      color="cyan darken-2"
-    >
-      <v-icon>
-        mdi-keyboard-return
-      </v-icon>
-    </v-btn>
+    <router-link to="/">
+      <v-btn class="mx-2" fab dark color="cyan darken-2">
+        <v-icon> mdi-keyboard-return </v-icon>
+      </v-btn>
     </router-link>
+
+    <div class="small">
+      <chart-container-vue />
+    </div>
+
   </h1>
 </template>
 
 <style scoped>
-.v-btn{
+.v-btn {
   left: 70%;
   top: 700px;
 }
 </style>
 
 <script>
+import chartContainerVue from "../components/chart-container.vue";
+
 export default {
+  components: { chartContainerVue },
   name: "Detail",
   props: {
     city: {
