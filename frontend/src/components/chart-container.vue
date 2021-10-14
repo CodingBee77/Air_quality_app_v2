effaf33a67b6205ced8aaf0a6efa4d1ae580c646
CodingBee77 <00martyna.znajdek00@gmail.com>
2021-10-14 18:15:55 +0200

    get historic measurement data on frontend side

--- frontend/src/components/chart-container.vue
+++ frontend/src/components/chart-container.vue
@@ -7,61 +7,62 @@
 
 <script>
 import LineChart from "./chart.vue";
+import axios from "axios";
 
 export default {
   name: "LineChartContainer",
+  props: {
+    lat: {
+      type: String,
+    },
+    long: {
+      type: String,
+    },
+    cityName: {
+      type: String,
+    },
+  },
   components: { LineChart },
   data: () => ({
     loaded: false,
     chartdata: null,
   }),
-
   async mounted() {
     this.loaded = false;
     try {
-      // const { historic_measurements } = await fetch('https://airapi.airly.eu/v2/measurements/nearest?lat=50.062006&lng=19.940984&maxDistanceKM=2&maxResults=1')
-      // historic_measurements_PM1 = historic_measurements["history"][1]["values"][1]["value"]
-      // this.chartdata = historic_measurements
-      this.chartdata = {
-        labels: ["January", "February", "March"],
-        datasets: [
-          {
-            label: "PM1",
-            backgroundColor: "#51B8A9A8",
-            data: [40, 20, 30],
-          },
-          {
-            label: "PM25",
-            backgroundColor: "#5FA833A8",
-            data: [50, 10, 60],
-          },
-        ],
-      };
-      this.options = {
-        // responsive: false,
-        maintainAspectRatio: false,
-        title: {
-          display: true,
-          text: "History air quality data - 1 DAY",
-        },
-        tooltips: {
-          callbacks: {
-            label(tooltipItem, chartdata) {
-              // Get the dataset label.
-              const label = chartdata.datasets[tooltipItem.datasetIndex].label;
-
-              // Format the y-axis value.
-              const value = tooltipItem.yLabel;
-
-              return `${label}: ${value}`;
-            },
-          },
-        },
-      };
-      this.loaded = true;
+      const response = await axios.get(
+        "http://localhost:5000/api/v1/measurements/history/?lat=" +
+          this.lat +
+          "&long=" +
+          this.long
+      );
+      this.chartdata = response.data;
+      console.log(this.chartdata);
     } catch (e) {
       console.error(e);
     }
+    this.options = {
+      // responsive: false,
+      maintainAspectRatio: false,
+      title: {
+        display: true,
+        text: "History air quality data - 1 DAY",
+      },
+      tooltips: {
+        callbacks: {
+          label(tooltipItem, chartdata) {
+            // Get the dataset label.
+            const label = chartdata.datasets[tooltipItem.datasetIndex].label;
+
+            // Format the y-axis value.
+            const value = tooltipItem.yLabel;
+
+            return `${label}: ${value}`;
+          },
+        },
+      },
+    };
+    this.loaded = true;
   },
 };
 </script>
