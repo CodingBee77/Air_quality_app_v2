b74e7e302690d184f67b062051a94670a478663b
CodingBee77 <00martyna.znajdek00@gmail.com>
2021-10-14 15:12:39 +0200

    Add method for fetching historic data for chart

--- backend/app/repositories/measurements_repository.py
+++ backend/app/repositories/measurements_repository.py
@@ -1,5 +1,5 @@
-from app.models import CurrentMeasurement
-from app.utils.obj_mapper import ObjectMapper
+from app.models import CurrentMeasurement, HistoricMeasurement
+from app.mappers.obj_mapper import ObjectMapper
 import requests
 from app.config import settings
 
@@ -8,27 +8,29 @@ class MeasurementRepository:
     def __init__(self):
         pass
 
+#TODO: create API client
+    #  def get_current_measurement_by_coordinates(self, lat: float, long: float) -> CurrentMeasurement:
+    #      mesurement_response = client.get_measurement(lat, lang)
+    #      current_meas = ObjectMapper.map_current_measurement(mesurement_response)
+    #      return current_meas
+
     def get_current_measurement_by_coordinates(self, lat: float, long: float) -> CurrentMeasurement:
         api_request = requests.get(
             "https://airapi.airly.eu/v2/measurements/nearest?lat=" +
             str(lat) + "&lng=" + str(long) + "&maxDistanceKM=2&maxResults=1",
             headers={'apikey': settings.AIRLY_API_KEY})
-        print(api_request)
-        current_measurement = ObjectMapper.map_current_measurement(api_request)
+        # print(api_request)
+        mapper = ObjectMapper()
+        current_measurement = mapper.map_current_measurement(api_request)
         return current_measurement
 
-    # def get_user_by_email(self, email: str):
-    #     with self.session_factory() as session:
-    #         return session.query(User).filter(User.email == email).first()
 
-    # def get_users(self, skip: int = 0, limit: int = 100):
-    #     with self.session_factory() as session:
-    #         return session.query(User).offset(skip).limit(limit).all()
-
-    # def create_user(self, user: UserCreate):
-    #     with self.session_factory() as session:
-    #         db_user = User(email=user.email)
-    #         session.add(db_user)
-    #         session.commit()
-    #         session.refresh(db_user)
-    #         return db_user
+    def get_historic_measurement_by_coordinates(self, lat: float, long: float) -> HistoricMeasurement:
+        api_request = requests.get(
+            "https://airapi.airly.eu/v2/measurements/nearest?lat=" +
+            str(lat) + "&lng=" + str(long) + "&maxDistanceKM=2&maxResults=1",
+            headers={'apikey': settings.AIRLY_API_KEY})
+        # print(api_request)
+        mapper = ObjectMapper()
+        historic_measurement = mapper.map_historic_measurement(response=api_request)
+        return historic_measurement
