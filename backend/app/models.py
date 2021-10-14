5c303ebd6fbd62655d66eb8090255aa1d66ed8ba
CodingBee77 <00martyna.znajdek00@gmail.com>
2021-10-14 15:11:45 +0200

    Add models for historic measurements

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,3 +5,15 @@ class CurrentMeasurement(BaseModel):
     from_date: str
     till_date: str
     values: List[dict]
+
+
+class HistoricMeasurementIndex(BaseModel):
+    label: str
+    background_color: str
+    data: List[float]
+
+
+class HistoricMeasurement(BaseModel):
+    labels: List[str]
+    datasets: List[HistoricMeasurementIndex]
+
