0e8788c316e9d27aaaf4232c8a3bda8df19a8bfe
CodingBee77 <00martyna.znajdek00@gmail.com>
2021-10-14 18:17:31 +0200

    Send parametrs from detailed view to the chart component

--- frontend/src/views/Detail.vue
+++ frontend/src/views/Detail.vue
@@ -1,39 +1,41 @@
 <template>
-    <h1 class="pt-0">
+  <h1 class="pt-0">
+    <v-container class="blue lighten-5">
+      <v-row dense>
+        <v-spacer>
+          <v-card class="pa-0" outlined tile>
+            <div>
+              <chart-container-vue
+                :lat="this.lat"
+                :long="this.long"
+                :cityName="this.cityName"
+              />
+            </div>
+          </v-card>
+        </v-spacer>
+      </v-row>
 
-      <v-container class="blue lighten-5">
-        <v-row dense>
-          <v-spacer>
-            <v-card class="pa-0" outlined tile >
-              <div >
-                <chart-container-vue />
-              </div>
-            </v-card>
-          </v-spacer>
-        </v-row>
+      <v-row dense>
+        <v-col v-for="n in 6" :key="n" cols="sm">
+          <v-card class="pa-16" outlined tile raised> col </v-card>
+        </v-col>
+      </v-row>
 
-        <v-row dense>
-          <v-col v-for="n in 6" :key="n" cols="sm">
-            <v-card class="pa-16" outlined tile raised> col </v-card>
+      <v-row dense style="height: 240px">
+        <v-spacer>
+          <v-col cols="12">
+            <v-card class="pa-16" outlined tile> Column </v-card>
           </v-col>
-        </v-row>
+        </v-spacer>
+      </v-row>
+    </v-container>
 
-        <v-row dense style="height: 240px">
-          <v-spacer>
-            <v-col cols="12">
-              <v-card class="pa-16" outlined tile> Column </v-card>
-            </v-col>
-          </v-spacer>
-        </v-row>
-      </v-container>
-
-      <router-link to="/">
-        <v-btn class="mx-2" fab dark color="cyan darken-2">
-          <v-icon> mdi-keyboard-return </v-icon>
-        </v-btn>
-      </router-link>
-
-    </h1>
+    <router-link to="/">
+      <v-btn class="mx-2" fab dark color="cyan darken-2">
+        <v-icon> mdi-keyboard-return </v-icon>
+      </v-btn>
+    </router-link>
+  </h1>
 </template>
 
 <style scoped>
@@ -50,9 +52,14 @@ export default {
   components: { chartContainerVue },
   name: "Detail",
   props: {
-    city: {
+    lat: {
       type: String,
-      default: 0,
+    },
+    long: {
+      type: String,
+    },
+    cityName: {
+      type: String,
     },
   },
 };
