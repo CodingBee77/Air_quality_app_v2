b4e30375d9108dd7b273bc32ab034c9e59b10d48
CodingBee77 <00martyna.znajdek00@gmail.com>
2021-10-14 18:12:00 +0200

    change chart color variable name

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from typing import List
 
+
 class CurrentMeasurement(BaseModel):
     from_date: str
     till_date: str
@@ -9,11 +10,10 @@ class CurrentMeasurement(BaseModel):
 
 class HistoricMeasurementIndex(BaseModel):
     label: str
-    background_color: str
+    backgroundColor: str
     data: List[float]
 
 
 class HistoricMeasurement(BaseModel):
     labels: List[str]
     datasets: List[HistoricMeasurementIndex]
-
