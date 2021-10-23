39deb8fe0e99632f4f48b8d9ded9be3073f6dcc6
CodingBee77 <00martyna.znajdek00@gmail.com>
2021-10-23 18:12:38 +0200

    Add form component to check air view

--- frontend/src/views/Checkair.vue
+++ frontend/src/views/Checkair.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="check air">
-    <h1>This is check air page.</h1>
+  <div id="app" class="container mt-5 mb-5" style="width: 500px">
+    <form-validation />
   </div>
 </template>
+
+
+<script>
+import FormValidation from "../components/form-validation.vue";
+
+export default {
+  name: "Check air",
+  components : {
+    FormValidation
+  },
+  data: () => ({
+    loaded: false,
+    chartdata: null,
+  }),
+  async mounted() {
+    try {
+      const response = await axios.get(
+        "http://localhost:5000/api/v1/measurements/current/?lat=" +
+          this.lat +
+          "&long=" +
+          this.long
+      );
+      this.measurements = response.data;
+      console.log(this.measurements);
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  }
+</script>
