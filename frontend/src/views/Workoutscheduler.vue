837f3f443c243264d5b645ccea1f3d71509744ef
CodingBee77 <00martyna.znajdek00@gmail.com>
2021-11-09 15:43:55 +0100

    Add example calendar

--- frontend/src/views/Workoutscheduler.vue
+++ frontend/src/views/Workoutscheduler.vue
@@ -1,5 +1,45 @@
 <template>
-  <div class="Workout scheduler">
-    <h1>This is workout scheduler page.</h1>
-  </div>
+  <v-row>
+    <v-col>
+      <v-sheet height="600">
+        <v-calendar
+          ref="calendar"
+          :now="today"
+          :value="today"
+          :events="events"
+          color="primary"
+          type="week"
+          class="mx-auto mt-14"
+        ></v-calendar>
+      </v-sheet>
+    </v-col>
+  </v-row>
 </template>
+
+
+<script>
+  export default {
+    data: () => ({
+      today: '2019-01-08',
+      events: [
+        {
+          name: 'Weekly Meeting',
+          start: '2019-01-07 09:00',
+          end: '2019-01-07 10:00',
+        },
+        {
+          name: `Thomas' Birthday`,
+          start: '2019-01-10',
+        },
+        {
+          name: 'Mash Potatoes',
+          start: '2019-01-09 12:30',
+          end: '2019-01-09 15:30',
+        },
+      ],
+    }),
+    mounted () {
+      this.$refs.calendar.scrollToTime('08:00')
+    },
+  }
+</script>
